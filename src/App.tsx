13099202f8fd77ea0edf01ef747a8e9965eb7cca
Oleksandr <askuznetsov6996@gmail.com>
2024-05-23 22:18:56 +0300

    added closing by blackspaces

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,17 @@ interface ModalProps {
 }
 
 const Modal: React.FC<ModalProps> = ({ onClose, className, children }) => {
+  const handleOverlayClick = (event: React.MouseEvent) => {
+    onClose();
+  };
+
+  const handleContentClick = (event: React.MouseEvent) => {
+    event.stopPropagation();
+  };
+
   return (
-    <div className={`modal ${className}`}>
-      <div className='modal-content'>
+    <div className={`modal ${className}`} onClick={handleOverlayClick}>
+      <div className='modal-content' onClick={handleContentClick}>
         <span className='close' onClick={onClose}>
           &times;
         </span>
@@ -44,7 +52,7 @@ const App: React.FC = () => {
     <div className='App'>
       <h1>React Modal App</h1>
       <div className='content'>
-        Greetings, my dear friend! <br /> I'm just a dumb message to fullfil an
+        Greetings, my dear friend! <br /> I'm just a dumb message to fulfill an
         empty space. Here is my sad story.{" "}
         <strong>Click the button below</strong> please, to give sense to my
         life.
@@ -56,10 +64,11 @@ const App: React.FC = () => {
         <Modal onClose={closeModal} className='modal-open'>
           <p>I've asked one AI how it sees Armar Solutions. Here is it.</p>
           <img
-            src='/Armar_solutions_png.png'
+            src='/Armar_solutions.png'
             alt='Armar Solutions AI generated pic'
             className='modal-image'
           />
+          <div></div>
           <button onClick={openNestedModal}>Open another modal window</button>
         </Modal>
       )}
@@ -67,10 +76,11 @@ const App: React.FC = () => {
         <Modal onClose={closeNestedModal} className='modal-nested'>
           <p>Then I asked it to redraw.</p>
           <img
-            src='/Armar_solutions_png_2.png'
+            src='/Armar_solutions_2.png'
             alt='Another Armar Solutions AI generated pic'
             className='modal-image'
           />
+          <div></div>
           <button onClick={closeNestedModal}>Close</button>
         </Modal>
       )}
